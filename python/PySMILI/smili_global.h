319aa5bc48d54ed11a4f8ca3b95c700db56fe754
Shakes Chandra (x14) <shekhar.chandra@uq.edu.au>
2023-11-02 06:33:09 +1000

    Python bindings now run! Fixed configs so that simple Python script runs Added QVTKWidget to allow access to QWidget in python script Minimal bindings config made

--- python/PySMILI/smili_global.h
+++ python/PySMILI/smili_global.h
@@ -1,24 +1,37 @@
 /*
   This file is part of SMILI.
 */
+#ifndef PYTHON_BINDINGS 
+#define PYTHON_BINDINGS
 
 #pragma once
 
 // Make "signals:", "slots:" visible as access specifiers
 #define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
 
-// Define PYTHON_BINDINGS
-#define PYTHON_BINDINGS
+//VTK
+#if VTK_MAJOR_VERSION <= 8
+  #include <QVTKWidget.h>
+#else
+  #include <QVTKOpenGLStereoWidget.h>
+#endif
+
+#if VTK_MAJOR_VERSION > 8
+  typedef QVTKOpenGLStereoWidget QVTKWidget;
+#endif
 
 //SMILI
-#include "milxMath.h"
-#include "milxModel.h"
-#include "milxFile.h"
+//#include "milxMath.h"
+// #include "milxModel.h"
+// #include "milxFile.h"
 //milxQt
 #include "milxQtWindow.h"
+#include "milxQtUnifiedWindow.h"
 #include "milxQtRenderWindow.h"
 #include "milxQtModel.h"
 #include "milxQtImage.h"
 #include "milxQtFile.h"
-#include "milxQtMain.h"
+#include "milxQtPlot.h"
+// #include "milxQtMain.h"
 
+#endif // Define PYTHON_BINDINGS
