b9a1b19aa2211b8cff39fa117aaf23cced7ff122
Shekhar S. Chandra <shekhar.chandra@uq.edu.au>
2025-09-01 16:25:49 +1000

    Added polydata to bindings, partially functional

--- python/PySMILI/smili_global.h
+++ python/PySMILI/smili_global.h
@@ -17,9 +17,10 @@
 #include <vtkSmartPointer.h>
 // #include <vtkMatrix4x4.h>
 // #include <vtkDataObject.h>
-//#include <vtkPoints.h>
+#include <vtkObject.h>
+#include <vtkPoints.h>
 //#include <vtkPointSet.h> //base of vtkPolyData
-// #include <vtkPolyData.h>
+#include <vtkPolyData.h>
 // #include <vtkImageData.h>
 #if VTK_MAJOR_VERSION <= 8
   #include <QVTKWidget.h>
--- python/test_vtk_polydata.py
+++ python/test_vtk_polydata.py
@@ -2,10 +2,11 @@
 Test the Python SMILI bindings with polydata
 '''
 import sys
-import vtk
-from PySMILI import milxQtModel, milxQtFile
+from PySMILI import vtkPoints, vtkPolyData, milxQtModel, milxQtFile
 # from PySide2 import QtWidgets
 from PySide6 import QtWidgets
+
+filename = "Bunny.vtp"
 
 if __name__ == "__main__":
     #setup Qt app
@@ -15,30 +16,14 @@ if __name__ == "__main__":
     app.setOrganizationName("PySMILI")
     app.setApplicationName("PolyData Viewer")
 
-    model = milxQtModel()
-
-    points = vtk.vtkPoints()
-    points.InsertNextPoint(0.0, 0.0, 0.0)  # Vertex 0
-    points.InsertNextPoint(1.0, 0.0, 0.0)  # Vertex 1
-    points.InsertNextPoint(0.5, 1.0, 0.0)  # Vertex 2
-
-    # 2. Create a cell (in this case, a triangle) to connect the points
-    triangle = vtk.vtkTriangle()
-    triangle.GetPointIds().SetId(0, 0) # Connect point 0 to vertex 0 of the triangle
-    triangle.GetPointIds().SetId(1, 1) # Connect point 1 to vertex 1 of the triangle
-    triangle.GetPointIds().SetId(2, 2) # Connect point 2 to vertex 2 of the triangle
-
-    # 3. Create a vtkCellArray to store the cell(s)
-    cells = vtk.vtkCellArray()
-    cells.InsertNextCell(triangle)
+    fileIO = milxQtFile()
 
     # 4. Create vtkPolyData and set its points and cells
-    polydata = vtk.vtkPolyData()
-    polydata.SetPoints(points)
-    polydata.SetPolys(cells)
+    polydata = vtkPolyData()
+    fileIO.openModel(filename, polydata)
 
     model = milxQtModel(mainWindow)
-    model.SetInputData(polydata)
+    model.SetInput(polydata)
     model.generateModel()
     model.colourMapToJet()
     model.setWindowTitle("Model")
