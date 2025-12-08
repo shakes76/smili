a6eaae464805a77fdc85e21579e8a2b317640af0
Shakes Chandra (WhiteOut24) <shekhar.chandra@uq.edu.au>
2025-12-08 21:58:03 +1000

    Updated points Python test re-enabled VTK PolyData

--- python/test_points.py
+++ python/test_points.py
@@ -2,10 +2,9 @@
 Test the Python SMILI bindings with polydata
 '''
 import sys
-from PySMILI import vtkPolyData, milxQtModel, milxQtFile
-from PySide2 import QtWidgets
-
-filename = "femur.vtk"
+from PySMILI import milxQtModel, milxQtFile
+#from PySide2 import QtWidgets
+from PySide6 import QtWidgets
 
 if __name__ == "__main__":
     #setup Qt app
